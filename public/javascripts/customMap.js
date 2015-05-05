06b0c6fcbda940927c70e83540f9de9fec40480e
muchhalsagar88 <muchhalsagar88@gmail.com>
2015-05-05 16:24:34 -0700

    changed for modal

--- public/javascripts/customMap.js
+++ public/javascripts/customMap.js
@@ -4,13 +4,19 @@ $(document).ready(function() {
     		showLabels: true,
 
     		click: function(event, data) {
-			    $('#clicked-state')
-			      .text('You clicked: '+data.name);
+			    $('#clicked-state').text('You clicked: '+data.name);
+			    $('#openModal').click();
+			    $('#disp-temp').text($('#map-'+data.name).prop('data-temp'));
 			  },
 
 			currentMapUpdate: function(event,data){
 				console.log('Changed color of:'+data.name+' to:'+event.originalEvent.color);
-				 $('#'+data.name).css('fill',''+event.originalEvent.color);
+				$('#'+data.name).css('fill',''+event.originalEvent.color);
+				$('#map-'+data.name).prop('data-temp', event.originalEvent.temp);
+				$('#map-'+data.name).prop('data-humidity', event.originalEvent.humidity);
+				$('#map-'+data.name).prop('data-wind', event.originalEvent.wind);
+				//if(event.originalEvent.city)
+					//$('#map-'+data.name).prop('data-city', event.originalEvent.city);
 			}
 
     	});
