248dbba60847a479b8035d1610b1dd7ad7cf60ba
Rohan Chakravarthy <rohanc219@gmail.com>
2015-05-05 09:51:49 -0400

    testing bayes

---
+++ test-bayes.js
@@ -0,0 +1,36 @@
+var fs=require('fs');
+var WeatherFunctions=require('openweathermap-plugin');
+
+var WeatherModule=new WeatherFunctions();
+
+WeatherModule.createStateDictionary();
+WeatherModule.getHistoricDataForState();
+
+
+function createObject(temperature,humidity,windSpeed){
+
+	return {temp:temperature,humidity:humidity,windSpeed:windSpeed};
+}
+
+function trainBayes(bayes){
+	var array = fs.readFileSync("./new.txt").toString().split("\n");
+	var labels = array.shift();
+		
+	for(i in array){
+		var params = array[i].split(",");
+		if(params.length>1){
+			console.log(params);
+			var trainObj=createObject(params[1],params[2],params[3]);
+			bayes.train(trainObj,params[4]);
+		}
+	}	
+}
+
+var ss = require('simple-statistics');
+var bayes = ss.bayesian();
+trainBayes(bayes);
+
+console.log('**TESTING OUTPUT***');
+
+var testObj=createObject(6,7,3);
+console.log(bayes.score(testObj));
