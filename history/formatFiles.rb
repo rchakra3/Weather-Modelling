1bb610898f52db5dde3bed24b4a26a720dbd1244
Shanil Puri <spuri3@ncsu.edu>
2015-05-04 21:32:21 -0400

    Master: Updated formatter script to generate single concatinated file per city for all months from january 1st, 2014.

--- history/formatFiles.rb
+++ history/formatFiles.rb
@@ -1,24 +1,43 @@
+require 'fileutils'
 retStr = ""
 dirPath = "/home/spuri/csc591_final/Weather-Modelling/history/"
 files = Dir[dirPath+"*.txt"]
-puts files
 directory_name = dirPath + "mod"
 Dir.mkdir(directory_name) unless File.exists?(directory_name)
-files.each do |f|
-	puts f
-	File.open(f, "r") do |infile|
-		while (line = infile.gets)
-			retStr =""
-			ctr = 0
-			elems = line.split(",").map(&:strip)
-			elems.each do |b|
-				if ctr == 0 || ctr == 2 || ctr == 8 || ctr == 17 || ctr == 21
-					retStr += b+", "
+Dir[directory_name+"/*"].each do |file|
+  FileUtils.rm_rf file, :noop => false, :verbose => true
+end
+
+for i in 1..50 do
+	fileCnt = 0
+	for j in 1..17 do
+		city = "city"+ "#{i}"
+		files = Dir[dirPath+ city + "_Month" + "#{j}" + ".txt"]
+		lineCnt = 0
+		files.each do |f|
+			File.open(f, "r") do |infile|
+				while (line = infile.gets)
+					retStr = ""
+					if(fileCnt == 0 && lineCnt == 0)
+						retStr = "Date, Mean TemperatureF, Mean Humidity, Mean Wind SpeedMPH, Events"
+					elsif(lineCnt > 1)
+						retStr = ""
+						ctr = 0
+						elems = line.split(",").map(&:strip)
+						elems.each do |b|
+							if ctr == 0 || ctr == 2 || ctr == 8 || ctr == 17 || ctr == 21
+								retStr += b+", "
+							end
+							ctr+=1
+						end
+					end
+					lineCnt += 1
+					if !retStr.empty?
+						File.open(dirPath+"mod/" + "mod_" + city + ".txt", 'a') { |file| file.write(retStr+"\n") }
+					end
 				end
-				ctr+=1
+				fileCnt += 2
 			end
-			fileName = File.basename(f)
-			File.open(dirPath+"mod/" + "mod_" + fileName, 'a') { |file| file.write(retStr+"\n") }
 		end
 	end
 end
