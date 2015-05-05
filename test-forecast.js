04ac7454f67b58d15aa69322f36f7d1aeec029de
VINAY <frags.vinay@gmail.com>
2015-05-05 17:08:06 -0400

    Returning boolean

--- test-forecast.js
+++ test-forecast.js
@@ -1,6 +1,9 @@
 var WeatherFunctions=require('openweathermap-plugin');
 
 var WeatherModule=new WeatherFunctions();
-WeatherModule.createStateDictionary();
-var status = WeatherModule.getForecast("Washington");
-console.log(status);
+var dict = WeatherModule.createStateDictionary();
+for(var state in dict){
+	console.log(state);
+	var status = WeatherModule.getCurrentRainForecast(state);
+	console.log(status);
+}
