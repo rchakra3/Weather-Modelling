c1cc5a7752e4515213c73ca1aeabd7e030584aae
VINAY <frags.vinay@gmail.com>
2015-05-05 21:36:50 -0400

    Added more keys and fixed the history files in Final UI

--- forecast-test.js
+++ forecast-test.js
@@ -1,9 +1,10 @@
 var WeatherFunctions=require('openweathermap-plugin');
 
 var WeatherModule=new WeatherFunctions();
-var dict = WeatherModule.createStateDictionary();
+//WeatherModule.getHistoricDataForState();
+/*var dict = WeatherModule.createStateDictionary();
 for(var state in dict){
 	console.log(state);
 	var status = WeatherModule.getCurrentRainForecast(state);
 	console.log(status);
-}
+}*/
